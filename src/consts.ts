c4c52bab0396d36af373d788de1d35a34e328cc0
yhrhee <iyhrhee@gmail.com>
2024-04-01 16:38:14 +0900

    update

--- src/consts.ts
+++ src/consts.ts
@@ -15,19 +15,19 @@ export const WORK: Page = {
 
 // Blog Page
 export const BLOG: Page = {
-  TITLE: "Blog",
+  TITLE: "查经",
   DESCRIPTION: "Writing on topics I am passionate about.",
 }
 
 // Projects Page 
 export const PROJECTS: Page = {
-  TITLE: "Projects",
+  TITLE: "资源",
   DESCRIPTION: "Recent projects I have worked on.",
 }
 
 // Search Page
 export const SEARCH: Page = {
-  TITLE: "Search",
+  TITLE: "查找",
   DESCRIPTION: "Search all posts and projects by keyword.",
 }
 
@@ -56,26 +56,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "yhmedia@proton.me",
+    HREF: "mailto:yhmedia@proton.mem",
   },
   { 
     NAME: "Github",
     ICON: "github",
     TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
-  },
-  { 
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    HREF: "https://github.com/yhrhee"
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
     TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://twitter.com/iyhrheev",
   },
 ]
 
